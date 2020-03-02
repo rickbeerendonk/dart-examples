5409dd465b4a55c18b561a0ffc96e681141c8d94
Rick Beerendonk <rick@beerendonk.com>
2020-03-02 16:34:49 +0100

    Add "Variables - Initial" example.

---
+++ 3. Variables/1 - Initial.dart
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+main() {
+  // Variable (of type integer)
+  int v;
+  print(v);
+}
+
+// null
