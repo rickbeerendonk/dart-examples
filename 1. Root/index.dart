f749b69d0deef0695584375e8003df8f6b02a641
Rick Beerendonk <rick@beerendonk.com>
2020-03-02 14:33:03 +0100

    Add copyright and result.

--- 1. Root/index.dart
+++ 1. Root/index.dart
@@ -1,4 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
 main() {
   // Print to the console
   print('Hello Dart!');
 }
+
+// Hello Dart!
