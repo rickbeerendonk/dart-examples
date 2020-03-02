9a103ca54441e31e97e173401a7ff3e769adc461
Rick Beerendonk <rick@beerendonk.com>
2020-03-02 16:54:46 +0100

    Add "Variables - Declarations" example.

---
+++ 3. Variables/2-declarations.dart
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+main() {
+  // Variable declarations
+  var v1 = 'Dart';
+  print(v1);
+
+  dynamic v2 = 'Dart';
+  print(v2);
+
+  String v3 = 'Dart';
+  print(v3);
+}
+
+// Dart
+// Dart
+// Dart
