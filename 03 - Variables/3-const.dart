8d081c5b9a71bc430380f844f01fc4ade2c9575a
Rick Beerendonk <rick@beerendonk.com>
2020-03-02 19:24:04 +0100

    Add "Variables - Const" example.

---
+++ 03 - Variables/3-const.dart
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+main() {
+  // Const declarations
+  const v1 = 'Dart';
+  print(v1);
+
+  const String v2 = 'Dart';
+  print(v2);
+
+  // Uncomment = Error: Setter not found: 'v1'
+  // v1 = 'Changed';
+}
+
+// Dart
+// Dart
