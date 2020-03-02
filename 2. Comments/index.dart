5b97a9492fb3bb48c71c1f95a406afa80760a8d5
Rick Beerendonk <rick@beerendonk.com>
2020-03-02 14:35:17 +0100

    Add "Comments" example.

---
+++ 2. Comments/index.dart
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+main() {
+  /* 
+  Multi line
+  comment
+  */
+  print('Hello Dart!'); // Single line comment
+}
