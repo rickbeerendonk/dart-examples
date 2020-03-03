043002c08ceddbbd44178268dc5b41237da2520c
Rick Beerendonk <rick@beerendonk.com>
2020-03-03 16:28:44 +0100

    Add "Build-in Types - Double" example.

---
+++ 04 - Build-in Types - TODO/03-double.dart
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+main() {
+  // Variable
+  var v1 = 0.123;
+  print("${v1} (${v1.runtimeType})");
+
+  double v2 = 0.456;
+  print("${v2} (${v2.runtimeType})");
+}
+
+// 0.123 (double)
+// 0.456 (double)
