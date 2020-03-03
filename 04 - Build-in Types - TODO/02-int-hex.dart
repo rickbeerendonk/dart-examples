5e7b9f8c8bf9ac3a6e465c3c5cbbc9b123fe3ade
Rick Beerendonk <rick@beerendonk.com>
2020-03-03 16:27:47 +0100

    Add "Build-in Types - Int Hex" example.

---
+++ 04 - Build-in Types - TODO/02-int-hex.dart
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+main() {
+  // Variable
+  var v1 = 0xABC;
+  print("${v1} (${v1.runtimeType})");
+
+  int v2 = 0xDEF;
+  print("${v2} (${v2.runtimeType})");
+}
+
+// 2748 (int)
+// 3567 (int)
