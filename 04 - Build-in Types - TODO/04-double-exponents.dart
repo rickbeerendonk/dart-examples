8878f07b2fc33ca0de9a85edc2f192a94bf4e620
Rick Beerendonk <rick@beerendonk.com>
2020-03-03 21:02:44 +0100

    Add "Build-in Types - Double Exponents" example.

---
+++ 04 - Build-in Types - TODO/04-double-exponents.dart
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+main() {
+  // Variable
+  var v1 = 1e10;
+  print("${v1} (${v1.runtimeType})");
+
+  double v2 = 2e20;
+  print("${v2} (${v2.runtimeType})");
+}
+
+// 10000000000.0 (double)
+// 200000000000000000000.0 (double)
