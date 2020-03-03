e6b921b4c5d178472920d78b6e57496fc96d7d48
Rick Beerendonk <rick@beerendonk.com>
2020-03-03 16:27:31 +0100

    Add type info.

--- 04 - Build-in Types - TODO/01-int.dart
+++ 04 - Build-in Types - TODO/01-int.dart
@@ -4,19 +4,19 @@
 main() {
   // Variable
   var v1 = 1;
-  print(v1);
+  print("${v1} (${v1.runtimeType})");
 
   int v2 = 2;
-  print(v2);
+  print("${v2} (${v2.runtimeType})");
 
   v1 = 3;
-  print(v1);
+  print("${v1} (${v1.runtimeType})");
 
   // Uncomment = Error: A value of type 'String' can't be assigned to a variable of type 'int'.
   //v1 = '3';
   //print(v1);
 }
 
-// 1
-// 2
-// 3
+// 1 (int)
+// 2 (int)
+// 3 (int)
