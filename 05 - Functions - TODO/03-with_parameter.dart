22136a36deeffdb7bef7dc78e73b02f40a41ed0f
Rick Beerendonk <rick@beerendonk.com>
2020-03-07 20:34:56 +0100

    Add "Functions - With Parameter" example.

---
+++ 05 - Functions - TODO/03-with_parameter.dart
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+main() {
+  // Function
+  foo(int x) {
+    return x * x;
+  }
+
+  print("${foo(2)} (${foo.runtimeType})");
+}
+
+// Closure: (int) => int -- 4 ((int) => int)
