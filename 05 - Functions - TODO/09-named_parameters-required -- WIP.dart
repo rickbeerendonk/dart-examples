b9d36f7d4880062d743bcbb1f1dd6adbd915ac1e
Rick Beerendonk <rick@beerendonk.com>
2020-03-15 20:33:00 +0100

    Add WIP.

---
+++ 05 - Functions - TODO/09-named_parameters-required -- WIP.dart
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import 'package:meta/meta.dart';
+
+main() {
+  // Named required parameters
+  int foo({int x1 = 2, @required int x2}) => x1 * x2;
+
+  print(foo(x2: 4));
+
+  // Omit required parameter x2
+  print(foo());
+}
+
+// 8 (({int x1, int x2}) => int)
