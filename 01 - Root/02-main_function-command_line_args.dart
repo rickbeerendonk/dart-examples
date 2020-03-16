c9b15399479ac5b7198f33e938a30c5565e614c5
Rick Beerendonk <rick@beerendonk.com>
2020-03-16 19:51:45 +0100

    Add "Root - Main Function - Command Line Arguments" example.

---
+++ 01 - Root/02-main_function-command_line_args.dart
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+// Main function: With command line arguments
+main(List<String> arguments) {
+  print(arguments);
+
+  print('Hello Dart!');
+}
+
+// [one, two, three]   // > dart 02-main_function-command_line_args.dart one two three
+// Hello Dart!
