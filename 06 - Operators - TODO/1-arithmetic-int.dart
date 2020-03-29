8773bfaf22a0ed7f7f4cd20a1a745fbeb681c05d
Rick Beerendonk <rick@beerendonk.com>
2020-03-29 16:48:43 +0200

    Add "Operators - Arithmetic - Int" example.

---
+++ 06 - Operators - TODO/1-arithmetic-int.dart
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+main() {
+  print("int: 7 + 2 = ${7 + 2}"); // 9
+  print("int: 7 - 2 = ${7 - 2}"); // 5
+  print("int: 7 * 2 = ${7 * 2}"); // 14
+  print("int: 7 / 2 = ${7 / 2} (double)"); // 3.5 (double)
+  print("int: 7 ~/ 2 = ${7 ~/ 2} (int)"); // 3 (int)
+  print("int: 7 % 2 = ${7 % 2}"); // 1
+}
