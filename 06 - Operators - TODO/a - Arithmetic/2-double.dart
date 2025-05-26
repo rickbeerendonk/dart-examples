b5e4f65f2e931c5316c23bf732cd15798784e8fd
Rick Beerendonk <rick@beerendonk.com>
2025-05-26 17:25:13 +0200

    Add "Arithmetic - Double" example.

---
+++ 06 - Operators - TODO/a - Arithmetic/2-double.dart
@@ -0,0 +1,10 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+void main() {
+  print("Double: 7.0 + 2.0: ${7.0 + 2.0}");  // 9.0
+  print("Double: 7.0 - 2.0: ${7.0 - 2.0}");  // 5.0
+  print("Double: 7.0 * 2.0: ${7.0 * 2.0}");  // 14.0
+  print("Double: 7.0 / 2.0: ${7.0 / 2.0}");  // 3.5
+  print("Double: 7.0 % 2.0: ${7.0 % 2.0}");  // 1.0
+}
