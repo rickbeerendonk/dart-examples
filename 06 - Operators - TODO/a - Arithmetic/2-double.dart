0dc788d1cf4abd608e6d432a2347423aa0fd17f5
Rick Beerendonk <rick@beerendonk.com>
2025-05-26 17:31:15 +0200

    Lower case type name.

--- 06 - Operators - TODO/a - Arithmetic/2-double.dart
+++ 06 - Operators - TODO/a - Arithmetic/2-double.dart
@@ -2,9 +2,9 @@
 // Copyright © 2022 Rick Beerendonk
 
 void main() {
-  print("Double: 7.0 + 2.0: ${7.0 + 2.0}");  // 9.0
-  print("Double: 7.0 - 2.0: ${7.0 - 2.0}");  // 5.0
-  print("Double: 7.0 * 2.0: ${7.0 * 2.0}");  // 14.0
-  print("Double: 7.0 / 2.0: ${7.0 / 2.0}");  // 3.5
-  print("Double: 7.0 % 2.0: ${7.0 % 2.0}");  // 1.0
+  print("double: 7.0 + 2.0: ${7.0 + 2.0}"); // 9.0
+  print("double: 7.0 - 2.0: ${7.0 - 2.0}"); // 5.0
+  print("double: 7.0 * 2.0: ${7.0 * 2.0}"); // 14.0
+  print("double: 7.0 / 2.0: ${7.0 / 2.0}"); // 3.5
+  print("double: 7.0 % 2.0: ${7.0 % 2.0}"); // 1.0
 }
