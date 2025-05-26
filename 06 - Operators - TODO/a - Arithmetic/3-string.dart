81618a43f07332e988b6d9d58d604be87e0a6731
Rick Beerendonk <rick@beerendonk.com>
2025-05-26 17:32:25 +0200

    Add "Arithmetic - String" example.

---
+++ 06 - Operators - TODO/a - Arithmetic/3-string.dart
@@ -0,0 +1,7 @@
+// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+void main() {
+  var str = "a" + "b";
+  print('String: "a" + "b": $str'); // String: "a" + "b": ab
+}
