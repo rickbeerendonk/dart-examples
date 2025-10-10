46b827690b1521c51fca56cc146aaecce6ed813c
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 10:55:45 +0200

    Add "Operators - Equality and Relational" example.

---
+++ 06 - Operators - TODO/d - Equality and Relational/1-equality_relational.dart
@@ -0,0 +1,8 @@
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+void main() {
+  int a = 5, b = 5;
+  print('a == b: ${a == b}'); // a == b: true
+  print('a != b: ${a != b}'); // a != b: false
+}
