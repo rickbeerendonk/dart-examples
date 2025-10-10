a1d7232cbd8859fa96b56e7d2b27d69b7699bf81
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 11:09:33 +0200

    Add "Operators - Type Test" example.

---
+++ 06 - Operators - TODO/e - Type Test/1-type_test.dart
@@ -0,0 +1,8 @@
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+void main() {
+  var x = 'hello';
+  print('x is String: ${x is String}'); // true
+  print('x is! int: ${x is! int}'); // true
+}
