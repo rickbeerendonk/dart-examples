97311a26b3a754b1e4387edbdc22f4d43afd2166
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 10:15:55 +0200

    Add "Core - Print" example.

---
+++ 16 - Core Libraries - TODO/a. Core/01-print.dart
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+// Library: dart:core
+
+void main() {
+  print('print() is in the dart/core library.');
+  print(111);
+  print(2.22);
+}
+
+// print() is in the dart/core library.
+// 111
+// 2.22
