f6ba0d956ef772c905cbda38d05f2bad91cad153
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 14:05:36 +0200

    Add "Collections - list - removeAt" example.

---
+++ 04c - Collections - TODO/a - List/09-removeAt.dart
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var list = [1, 2, 3];
+
+  list.removeAt(1); // removes element at index 1
+
+  print(list);
+}
+
+// [1, 3]
