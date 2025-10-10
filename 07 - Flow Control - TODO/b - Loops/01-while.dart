ce4f96ab041e628cd94f761147a106d57c18768e
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 23:16:34 +0200

    Add "Flow Control - Loops - While" example.

---
+++ 07 - Flow Control - TODO/b - Loops/01-while.dart
@@ -0,0 +1,19 @@
+// European Union Public License version 1.2
+// Copyright © 2023-2025 Rick Beerendonk
+
+void main() {
+  var i = 1;
+
+  while (i < 5) {
+    print('Iteration $i');
+    i++;
+  }
+
+  print('End');
+}
+
+// Iteration 1
+// Iteration 2
+// Iteration 3
+// Iteration 4
+// End
