5c6e4092d1c771791b7fc3fc609d711a5ab2477e
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 23:05:35 +0200

    Split code.

--- 05 - Functions - TODO/08-named_parameters.dart
+++ 05 - Functions - TODO/08-named_parameters.dart
@@ -1,12 +1,14 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-main() {
-  // Named parameters
-  int foo({int x1 = 2, int x2 = 3}) => x1 * x2;
+// Named parameters (always optional)
+int foo({int? x1, int? x2}) => (x1 ?? 0) + (x2 ?? 0);
 
+main() {
   // Only a non-default value for x2
-  print("${foo(x2: 4)} (${foo.runtimeType})");
+  print(foo(x2: 2));
+  print(foo.runtimeType);
 }
 
-// 8 (({int x1, int x2}) => int)
+// 2
+// ({int? x1, int? x2}) => int
