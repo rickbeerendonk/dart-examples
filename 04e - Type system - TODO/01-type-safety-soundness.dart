249a887bb826baf8f0f352a3baa8d63bda5c1614
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 14:47:16 +0200

    Add "Type system - Type Safety" example.

---
+++ 04e - Type system - TODO/01-type-safety-soundness.dart
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  String s = '123';
+  print(s);
+
+  // Uncommenting the next line would cause a compile-time error:
+  // Error: A value of type 'String' can't be assigned to a variable of type 'int'.
+  //int i = s;
+}
+
+// 123
