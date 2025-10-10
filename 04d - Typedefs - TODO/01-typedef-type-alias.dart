9c1a6a0fdc843aba96369e026cac09da520c4dac
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 11:42:48 +0200

    Add "Typedefs - Type Alias" example.

---
+++ 04d - Typedefs - TODO/01-typedef-type-alias.dart
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+typedef IntList = List<int>;
+
+void main() {
+  IntList il = [1, 2, 4];
+  print(il);
+}
+
+// [1, 2, 4]
