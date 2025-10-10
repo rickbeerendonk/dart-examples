87479a502d8394efe0b3771108af9f8ad427747e
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 11:29:54 +0200

    Add "Operators - Conditional Expressions" example.

---
+++ 06 - Operators - TODO/i - Conditional Expressions/1-conditional.dart
@@ -0,0 +1,14 @@
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+void main() {
+  int a = 1;
+  int b = 2;
+  String result;
+
+  result = a < b ? 't' : 'f';
+  print(result); // t
+
+  result = a > b ? 't' : 'f';
+  print(result); // f
+}
