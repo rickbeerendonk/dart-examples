f8384e30596a9f7428eb881da529f5c87ecb8d2f
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 14:17:50 +0200

    Add NOT.

--- 06 - Operators - TODO/h - Bitwise and Shift/1-bitwise_shift.dart
+++ 06 - Operators - TODO/h - Bitwise and Shift/1-bitwise_shift.dart
@@ -6,6 +6,7 @@ void main() {
   print('a & b: ${a & b}'); // 1
   print('a | b: ${a | b}'); // 7
   print('a ^ b: ${a ^ b}'); // 6
+  print('~a: ${~a}'); // -6
   print('a << 1: ${a << 1}'); // 10
   print('a >> 1: ${a >> 1}'); // 2
 }
@@ -30,6 +31,9 @@ a ^ b (XOR):
   0101
 ^ 0011
 = 0110 (6)
+
+~a (NOT,  inverts all bits, including the sign bit):
+  ~0101 = 1010 
 
 a << 1 (left shift):
   0101 << 1 = 1010 (10)
