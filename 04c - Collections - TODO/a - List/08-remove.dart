b7e7ef0049482b6db3900287d2f239a5ae0bf4a3
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 14:04:57 +0200

    Add "Collections - List - remove" example.

---
+++ 04c - Collections - TODO/a - List/08-remove.dart
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var list = [1, 2, 3, 2];
+
+  list.remove(2); // removes first occurrence of value 2
+
+  print(list);
+}
+
+// [1, 3, 2]
