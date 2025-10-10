f0ec5545a42641e37622f3ad0cabe0b3f31d5d35
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 23:22:13 +0200

    Add "Flow Control - Loops - Break Continue - While" example.

---
+++ 07 - Flow Control - TODO/b - Loops/05-break_continue-while.dart
@@ -0,0 +1,20 @@
+// European Union Public License version 1.2
+// Copyright © 2024-2025Rick Beerendonk
+
+void main() {
+  int i = 0;
+
+  while (i < 20) {
+    i++;
+    if (i % 3 == 0) continue;
+    if (i % 10 == 0) break;
+    print(i);
+  }
+}
+
+// 1
+// 2
+// 4
+// 5
+// 7
+// 8
