444bf50c097c9ec5225e7de1aec4c9190d65c8da
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 23:20:24 +0200

    Add "Flow Control - Loops - For In" example.

---
+++ 07 - Flow Control - TODO/b - Loops/04-for_in.dart
@@ -0,0 +1,15 @@
+// European Union Public License version 1.2
+// Copyright © 2023-2025 Rick Beerendonk
+
+void main() {
+  List<String> players = ["Rafael Nadal", "Roger Federer", "Serena Williams"];
+
+  // One line
+  for (String player in players) {
+    print(player);
+  }
+}
+
+// Rafael Nadal
+// Roger Federer
+// Serena Williams
