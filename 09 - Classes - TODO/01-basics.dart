892178432416929d8b08913ea2fd57186bcaadad
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 12:12:07 +0200

    Add "Classes - Basics" example.

---
+++ 09 - Classes - TODO/01-basics.dart
@@ -0,0 +1,26 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+import 'dart:math';
+
+class Point {
+  // Instance variables
+  double x;
+  double y;
+
+  // Constructor
+  Point(this.x, this.y);
+
+  // Instance method
+  double distanceTo(Point other) {
+    var dx = x - other.x;
+    var dy = y - other.y;
+    return sqrt(dx * dx + dy * dy);
+  }
+}
+
+void main() {
+  var p = Point(2, 3);
+  print(p.x); // 2.0
+  print(p.distanceTo(Point(3, 2))); // 1.4142135623730951
+}
