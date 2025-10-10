5897781efbad53aae9803468b05d0f8ab43bb39a
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 23:10:48 +0200

    Add "Functions - As Variable" example.

---
+++ 05 - Functions - TODO/13-function_as_variable.dart
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+var foo = () => "Foo";
+
+main() {
+  print(foo());
+  print(foo.runtimeType);
+}
+
+// Foo
+//() => String
