6d760dcdab44789123cd640090731be206954d1b
Rick Beerendonk <rick@beerendonk.com>
2025-10-11 16:32:09 +0200

    Add "Collections - Query - max" example.

---
+++ 04c - Collections - TODO/d - Query/12 - Aggregation/b - max.dart
@@ -0,0 +1,10 @@
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+import 'dart:math' as math;
+
+void main() {
+  List<int> numbers = [2, 1, 4, 3];
+
+  print(numbers.reduce(math.max)); // 4
+}
