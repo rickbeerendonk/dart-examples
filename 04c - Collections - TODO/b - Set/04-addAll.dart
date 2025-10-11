e086e8efaa1550f57d2344beab9acc2eed6cba28
Rick Beerendonk <rick@beerendonk.com>
2025-10-11 15:46:07 +0200

    Add "Collections - Set - addAll" example.

---
+++ 04c - Collections - TODO/b - Set/04-addAll.dart
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var set = {1, 2, 3};
+  set.addAll([97, 98, 99]);
+  print(set);
+}
+
+// {1, 2, 3, 97, 98, 99}
