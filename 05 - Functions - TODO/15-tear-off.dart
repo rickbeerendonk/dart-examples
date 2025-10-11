35802fcf27f8ccbaeabc8e9be42b14dd73a8fc27
Rick Beerendonk <rick@beerendonk.com>
2025-10-11 16:56:29 +0200

    Add "Functions - tear-off" example.

---
+++ 05 - Functions - TODO/15-tear-off.dart
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void printItem(int item) {
+  print(item);
+}
+
+void main() {
+  var list = [1, 2, 3];
+  list.forEach(printItem); // Function tear-off
+}
+// 1
+// 2
+// 3
