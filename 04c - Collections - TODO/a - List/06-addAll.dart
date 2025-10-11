51fd842d1f91046c477064881900aa2d217616a4
Rick Beerendonk <rick@beerendonk.com>
2025-10-11 15:48:32 +0200

    Add "Collections - List - addAll" example.

---
+++ 04c - Collections - TODO/a - List/06-addAll.dart
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var list = [1, 2, 3];
+  list.addAll([97, 98, 99]);
+  print(list);
+}
+
+// [1, 2, 3, 97, 98, 99]
