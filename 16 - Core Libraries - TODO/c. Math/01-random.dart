11b8c20344f2ba5ea40fbebd7141247b4b6cbf71
Rick Beerendonk <rick@beerendonk.com>
2025-10-12 00:20:39 +0200

    Add "Core Libraries - Math - random" examples.

---
+++ 16 - Core Libraries - TODO/c. Math/01-random.dart
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+import 'dart:math';
+
+void main() {
+  var random = Random();
+  print('Random double: ${random.nextDouble()}');
+  print('Random int (0-9): ${random.nextInt(10)}');
+  print('Random bool: ${random.nextBool()}');
+}
