d8d88a365de0e2acc7c854424d0cd652994f5a7d
Rick Beerendonk <rick@beerendonk.com>
2025-10-11 15:49:08 +0200

    Add "Collections - Set - remove" example.

---
+++ 04c - Collections - TODO/b - Set/05-remove.dart
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var set = {1, 2, 3};
+
+  set.remove(2); // removes value 2
+
+  print(set);
+}
+
+// {1, 3}
