ca4da1b92e973e84006aa36c7d7a5f8bd447a9cb
Rick Beerendonk <rick@beerendonk.com>
2025-10-11 15:53:40 +0200

    Add "Collections - List - filled" example.

---
+++ 04c - Collections - TODO/a - List/12-construct-filled.dart
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var list = List<int>.filled(3, 0);
+  print(list);
+}
+
+// [0, 0, 0]
