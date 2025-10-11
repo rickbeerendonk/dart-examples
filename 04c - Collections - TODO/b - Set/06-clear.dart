09f00c004528f97756ed6e5ee6f5da74d32f5ef3
Rick Beerendonk <rick@beerendonk.com>
2025-10-12 01:02:41 +0200

    Add "Collections - Set - clear" example.

---
+++ 04c - Collections - TODO/b - Set/06-clear.dart
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  Set<int> set = {1, 2, 3};
+  set.clear();
+  print(set);
+}
+
+// {}
