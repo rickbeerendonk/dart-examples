4a3fe87212e78fc0939b36b3f987b2571e0c258b
Rick Beerendonk <rick@beerendonk.com>
2025-10-11 16:34:43 +0200

    Add "Collections - Map - literal" example.

---
+++ 04c - Collections - TODO/c - Map/01-literal.dart
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var v1 = {'one': 1, 'two': 2, 'three': 3};
+  print(v1);
+
+  Map<String, int> v2 = {'one': 1, 'two': 2, 'three': 3};
+  print(v2);
+}
+
+// {one: 1, two: 2, three: 3}
+// {one: 1, two: 2, three: 3}
