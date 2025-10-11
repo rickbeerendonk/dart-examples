b630a474e3c7c97bd39d0ec49d5bdb5d5176408b
Rick Beerendonk <rick@beerendonk.com>
2025-10-11 15:54:33 +0200

    Add "Collections - List - spread" example.

---
+++ 04c - Collections - TODO/a - List/13-spread-element.dart
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var a = [1, 2, 3];
+  var list = [0, ...a, 4];
+  print(list); // [0, 1, 2, 3, 4]
+}
+
+// [0, 1, 2, 3, 4]
