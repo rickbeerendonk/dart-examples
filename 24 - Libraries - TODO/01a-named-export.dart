0d7f16fef61716d94767eafe23646f791d0f0176
Rick Beerendonk <rick@beerendonk.com>
2025-10-14 00:54:54 +0200

    Add "Libraries - names export/import" example.

---
+++ 24 - Libraries - TODO/01a-named-export.dart
@@ -0,0 +1,11 @@
+// European Union Public License version 1.2
+// Copyright © 2015-2025 Rick Beerendonk
+
+// Named exports in Dart are achieved by making functions and variables public
+// Public members start with lowercase letters (private members start with _)
+
+double square(double x) {
+  return x * x;
+}
+
+const double pi = 3.14;
---
+++ 24 - Libraries - TODO/01b-named-import.dart
@@ -0,0 +1,9 @@
+// European Union Public License version 1.2
+// Copyright © 2015-2025 Rick Beerendonk
+
+import '01a-named-export.dart';
+
+void main() {
+  print(square(3)); // 9.0
+  print(pi); // 3.14
+}
