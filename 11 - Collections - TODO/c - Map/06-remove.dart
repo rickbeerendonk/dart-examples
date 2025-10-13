81507b27ab1501e15c92f8296f3e8d164fa3448c
Rick Beerendonk <rick@beerendonk.com>
2025-10-13 19:51:51 +0200

    Add "Collections - Map - remove" example.

---
+++ 11 - Collections - TODO/c - Map/06-remove.dart
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var map = {'one': 1, 'two': 2, 'three': 3};
+  map.remove('two');
+  print(map);
+}
+
+// {one: 1, three: 3}
