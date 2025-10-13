c46b4cb28957c2bc4c0f17ed8509f901b23d1bda
Rick Beerendonk <rick@beerendonk.com>
2025-10-13 14:13:20 +0200

    Add limits and special values.

--- 04 - Build-in Types/05-double.dart
+++ 04 - Build-in Types/05-double.dart
@@ -2,13 +2,30 @@
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
 main() {
-  // Variable
+  // Impicit
   var v1 = 0.123;
   print("${v1} (${v1.runtimeType})");
 
+  // Explicit
   double v2 = 0.456;
   print("${v2} (${v2.runtimeType})");
+
+  // Limits
+  print("Max finite value: ${double.maxFinite}");
+  print("Min positive value: ${double.minPositive}");
+
+  // Special values
+  print("Positive infinity: ${double.infinity}");
+  print("Negative infinity: ${double.negativeInfinity}");
+  print("Not a Number: ${double.nan}");
 }
 
 // 0.123 (double)
 // 0.456 (double)
+
+// Max finite value: 1.7976931348623157e+308
+// Min positive value: 5e-324
+
+// Positive infinity: Infinity
+// Negative infinity: -Infinity
+// Not a Number: NaN
