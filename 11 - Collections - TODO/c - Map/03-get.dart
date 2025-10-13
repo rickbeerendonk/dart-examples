15a313893560a2448633aa99740f3b4430b3ee0b
Rick Beerendonk <rick@beerendonk.com>
2025-10-13 19:51:07 +0200

    Add "Collections - Map - get" example.

---
+++ 11 - Collections - TODO/c - Map/03-get.dart
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+// Output: Demonstrates getting a value from a Map.
+
+void main() {
+  var map = {'one': 1, 'two': 2, 'three': 3};
+  print(map['one']);
+  print(map['two']);
+  print(map['three']);
+}
+
+// 1
+// 2
+// 3
