6f440dba893bc23a459e785e2fdca173626bf295
Rick Beerendonk <rick@beerendonk.com>
2025-10-13 19:52:23 +0200

    Add "Collections - Map - constructor from" example.

---
+++ 11 - Collections - TODO/c - Map/09-constructor-from.dart
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var map = Map<String, int>.from({'one': 1, 'two': 2});
+  print(map);
+}
+
+// {one: 1, two: 2}
