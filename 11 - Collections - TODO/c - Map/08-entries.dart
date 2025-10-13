d17e558019431b83d91412be59922abcc744171b
Rick Beerendonk <rick@beerendonk.com>
2025-10-13 19:52:03 +0200

    Add "Collections - Map - entries" example.

---
+++ 11 - Collections - TODO/c - Map/08-entries.dart
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+void main() {
+  var v1 = {'one': 1, 'two': 2, 'three': 3};
+  print(v1);
+  print(v1.entries);
+}
+
+// {one: 1, two: 2, three: 3}
+// (MapEntry(one: 1), MapEntry(two: 2), MapEntry(three: 3))
